e36c3acfff2ed4d9df2af7fa50e28015d173b785
sam-mugo <keithgety@gmail.com>
2020-05-08 09:53:50 +0300

    Update scripts.js

--- Apps/Weather-App/scripts.js
+++ Apps/Weather-App/scripts.js
@@ -15,7 +15,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "3e5f836cee8b511213779b5df23ff1c5";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
